3af7ea515ff2a13e69e1c0eaa8ba0a7bf9616d4b
lewis21456 <lewislin21456@gmail.com>
2025-05-26 23:01:09 +0800

    0526更新-2

---
+++ scene/script/game_2.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+# 終點門和轉場
+@onready var Door = get_node("door")
+@onready var Transition = get_node("Transitiondoor")
+
+# 死亡
+#@onready var trapcoin = get_node("coin dead") # 陷阱節點
+#@onready var player = get_node("CharacterBody2D") # 角色
+
+func _ready() -> void:
+	Transition.play("dooropen")
+	Door.connect("enter" , Callable(self , "_on_enter"))  # 終點門信號連結
+	#trapcoin.connect("die" , Callable(self , "_on_die")) # 陷阱信號連結
+	pass
+
+func _on_enter() -> void:
+	Transition.play("doorclose")
+	await Transition.animation_finished
+	#轉場景
+	
+	get_tree().change_scene_to_file("res://scene/game_3.tscn")
+
+	pass

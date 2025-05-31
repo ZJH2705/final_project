69c895f23d2916f41e850f7f24421fce08e481f5
413410073鄭峻皓 <zjh2705@gmail.com>
2025-05-31 18:24:11 +0800

    設計spike關卡和新增spike.tscn

---
+++ scene/script/level2_zjh/spike.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+signal die
+
+@onready var timer: Timer = $Timer
+var has_triggered = false
+
+func _on_body_entered(body: Node2D) -> void:
+	if has_triggered:
+		return
+
+	if body is CharacterBody2D:
+		has_triggered = true
+		print("你已經死了")
+
+		var player = body as CharacterBody2D
+		player.die() #呼叫自己死亡
+		GameState.death_count+=1
+		print("hello")
+		await player.anim.animation_finished
+		timer.start()
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
+	emit_signal("die")
